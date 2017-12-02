14d357c09285f9298a13f7fa14bb098b26ee6b7f
Freedomson <xntp006@gmail.com>
2017-12-02 01:06:28 +0000

    2

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ var Config = {
   baseurl: "http://www.guganews.com",
   port : 8080,
   host : "0.0.0.0",
-  apikey      : "262c8a69-a337-4caa-882c-aa3426fbca02", //process.env.apikey || "342f4d9f-689d-48f1-b4bc-34cbdbb1745d",
-  apikeylang  : process.env.apikeylang || "trnsl.1.1.20171118T192403Z.5dae56c21f10dd02.340a8fc76bb2e6f96d02a831c3df210756adbb16", //"trnsl.1.1.20171108T205952Z.dd3888adeecde2e5.455eb8d908e862fff5308b33afecf931d87f3611",
+  apikey      : process.env.API_KEY,
+  apikeylang  : process.env.TRANS_KEY,
   cachetimeout     :  1000*60*60*24*10,
   cachetimeoutlong :  1000*60*60*24*10,
   keywords : {
